3032abb92698d527c21908d44faa41f9175e9241
Fukuda Naoto <naoto.fukuda1217@gmail.com>
2020-05-11 23:22:07 +0900

    Change S3_URL to IMAGE_URL

--- pages/index.vue
+++ pages/index.vue
@@ -20,9 +20,11 @@ export default {
   },
   methods: {
     async fetchEdamames() {
+      console.log('API_URL', process.env.API_URL)
+      console.log('IMAGE_URL', process.env.IMAGE_URL)
       const response = await this.$axios.$get(process.env.API_URL)
       this.edamames = response.edamames.map((edamame) => ({
-        imageUrl: `${process.env.S3_URL}/${edamame}`,
+        imageUrl: `${process.env.IMAGE_URL}/${edamame}`,
         date: Number(edamame.slice(0, -4)),
         month: Number(edamame.slice(2, 4)),
         day: Number(edamame.slice(4, 6))
